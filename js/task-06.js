b0883dd043c8db27578635b3afe8c5a648443a9e
IhorMarch <97352675+IhorMarch@users.noreply.github.com>
2023-06-15 23:37:12 +0300

    fixed task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,14 +1,21 @@
 
-const form = document.querySelector("input");
-
+const form = document.querySelector("#validation-input");
+console.log(form.dataset.length);
 
 form.addEventListener("blur", event => {
-   
-    if (event.currentTarget.value.length > form.dataset.length) {
-        
-        form.classList.add("invalid")
+    
+    
+    if (event.target.value.length === Number(form.dataset.length)) {
+         form.classList.remove('invalid');
+         form.classList.add('valid');
+    
     }
-    form.classList.add("valid")
-
+   
+    else {
+        form.classList.remove('valid');
+        form.classList.add('invalid');
+         
+  }
+		
+		
 });
-
